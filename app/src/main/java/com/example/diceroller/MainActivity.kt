c9b5a3a0583b1d3fb6e0c782cecc130cfb02162f
Android <gguptag18@gmail.com>
2022-08-25 18:33:46 +0530

    Revert "added some logic"
    This reverts commit 9189ab797fffc0ab07c29beaef53f5eaa618b074.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -11,14 +11,11 @@ import androidx.appcompat.app.AppCompatActivity
  * on the screen.
  */
 class MainActivity : AppCompatActivity() {
-
-    private var clickCount = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-
         // Set a click listener on the button to roll the dice when the user taps the button
         rollButton.setOnClickListener {
 //            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
@@ -29,38 +26,7 @@ class MainActivity : AppCompatActivity() {
 //            resultTextView.text = "6"
 
             rollDice()
-            clickCount++;
         }
-
-        rollButton.setOnClickListener {
-//            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-//            toast.show()
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-
-//            val resultTextView: TextView = findViewById(R.id.textView)
-//            resultTextView.text = "6"
-
-            rollDice()
-            clickCount++;
-        }
-        if(clickCount == 6){
-            //disbale player2 buttotn.setEnabled(false)
-            //diable player2
-
-            //calcWinner(player1Score, player2Score)
-
-        }
-    }
-
-    fun calcWinner(){
-
-        //winner calculated
-        showWinnerPopUp(winner);
-
-    }
-
-    fun showWinnerPopUp(int winner){
-        //pop
     }
 
     /**
@@ -68,29 +34,30 @@ class MainActivity : AppCompatActivity() {
      */
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
-        val dice1 = Dice(6)
-        val diceRoll = dice1.roll()
-        var sum1=0
-        var sum2=0
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
         // Update the screen with the dice roll
 //        val resultTextView: TextView = findViewById(R.id.textView)
 //        resultTextView.text = diceRoll.toString()
         val diceImage: ImageView =findViewById(R.id.imageView)
-        for (chance in 1..3){
-            if (diceRoll == 1) {
-                diceImage.setImageResource(R.drawable.dice_1)
-            } else if (diceRoll == 2) {
-                diceImage.setImageResource(R.drawable.dice_2)
-            } else if (diceRoll == 3) {
-                diceImage.setImageResource(R.drawable.dice_3)
-            } else if (diceRoll == 4) {
-                diceImage.setImageResource(R.drawable.dice_4)
-            } else if (diceRoll == 5) {
-                diceImage.setImageResource(R.drawable.dice_5)
-            } else {
-                diceImage.setImageResource(R.drawable.dice_6)
-            }
-            sum1 += diceRoll
+        if(diceRoll==1) {
+            diceImage.setImageResource(R.drawable.dice_1)
+        }
+        else if(diceRoll==2){
+            diceImage.setImageResource(R.drawable.dice_2)
+        }
+        else if(diceRoll==3){
+            diceImage.setImageResource(R.drawable.dice_3)
+        }
+        else if(diceRoll==4){
+            diceImage.setImageResource(R.drawable.dice_4)
+        }
+        else if(diceRoll==5){
+            diceImage.setImageResource(R.drawable.dice_5)
+        }
+        else{
+            diceImage.setImageResource(R.drawable.dice_6)
         }
         diceImage.contentDescription = diceRoll.toString()
     }
